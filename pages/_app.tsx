839427e93ed03477f5d870fb3462489455a19ece
JosephTseng <zxp930110@hotmail.com.tw>
2021-05-08 17:36:45 +0800

    add components

---
+++ pages/_app.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import '../styles/globals.css';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from '../styles/theme';
+import Header from '../src/views/layout/Header'
+import Navigation from '../src/views/layout/Navigation'
+
+function MyApp({ Component, pageProps }) {
+	useEffect(() => {
+		// Remove the server-side injected CSS.
+		const jssStyles = document.querySelector('#jss-server-side');
+		if (jssStyles) {
+			jssStyles.parentElement!.removeChild(jssStyles);
+		}
+	}, []);
+
+	return (
+		<React.Fragment>
+			<title>123</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<ThemeProvider theme={theme}>
+				<Navigation />
+				<Header />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</React.Fragment>
+	);
+}
+
+export default MyApp;
---
+++ styles/palette.js
@@ -0,0 +1,16 @@
+export const palette = {
+    primary: {
+        main: '#5A6B34',
+    },
+    secondary: {
+        main: '#D7B740',
+    },
+    background: {
+        default: '#AB8024',
+        paper: '#bfbfbf',
+    },
+    text: {
+        primary: '#2a2a2a',
+        secondary: '#ffffff',
+    },
+}
