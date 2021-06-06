1518c878e3ea125cbb6fa69a7a30379f1747803e
Joseph Tseng <zxp930110@hotmail.com.tw>
2021-06-07 01:24:20 +0800

    modify palette

--- styles/palette.js
+++ styles/palette.js
@@ -1,16 +1,18 @@
 export const palette = {
     primary: {
-        main: '#5A6B34',
+        light: '829DA9',
+        main: '#254251',
     },
     secondary: {
-        main: '#D7B740',
+        light: 'FA814A',
+        main: '#F04B00',
     },
     background: {
-        default: '#AB8024',
-        paper: '#bfbfbf',
+        default: '#F1ECE8',
+        paper: '#DFA680',
     },
     text: {
-        primary: '#2a2a2a',
-        secondary: '#ffffff',
+        primary: '#254251',
+        secondary: '#FFFFFF',
     },
 }
