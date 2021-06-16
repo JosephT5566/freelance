450b63586aa4cb4e77c15a1753e9b6f35edb168a
Joseph Tseng <zxp930110@hotmail.com.tw>
2021-06-17 01:05:31 +0800

    modify `viewpager` to infinite scroll

--- src/components/shared/viewPager.tsx
+++ src/components/shared/viewPager.tsx
@@ -146,6 +146,115 @@ export default function ViewPager(props: { pages: Page[] }) {
 	const containerRef = useRef(null);
 
 	const width = useWidth(containerRef);
+	const count = pages.length;
+	const offsetArray = useRef([0]);
+	offsetArray.current = pages.map((_, index) => (index < Math.ceil(count / 2) ? index : index - count));
+
+	const currentNext = (): number => (pageIndexRef.current + 1 > count - 1 ? 0 : pageIndexRef.current + 1);
+
+	const currentPrev = (): number => (pageIndexRef.current - 1 < 0 ? count - 1 : pageIndexRef.current - 1);
+
+	const [springs, api] = useSprings(
+		count,
+		(item) => ({
+			x: offsetArray.current[item] * width,
+			display: 'block',
+		}),
+		[width]
+	);
+
+	const apiStart = () => {
+		api.start((item) => {
+			const newPosition = (item - pageIndexRef.current + count) % count;
+			const x = offsetArray.current[newPosition] * width;
+			return item != currentNext() && item != currentPrev() && item != pageIndexRef.current
+				? { x, display: 'none' }
+				: { x, display: 'block' };
+		});
+	};
+
+	useEffect(() => {
+		const intervalID = setInterval(() => {
+			pageIndexRef.current = currentNext();
+			apiStart();
+			setDotIndex(pageIndexRef.current);
+		}, 10000);
+
+		return () => {
+			clearInterval(intervalID);
+		};
+	}, [apiStart, currentNext, setDotIndex]);
+
+	const bind = useDrag(
+		({ active, movement: [mx], direction: [xDir], distance, cancel }) => {
+			if (active && distance > width / 3) {
+				pageIndexRef.current = xDir > 0 ? currentPrev() : currentNext();
+				cancel();
+			}
+			api.start((item) => {
+				const newPosition = (item - pageIndexRef.current + count) % count;
+				const x = offsetArray.current[newPosition] * width + (active ? mx : 0);
+				return item != currentNext() && item != currentPrev() && item != pageIndexRef.current
+					? { x, display: 'none' }
+					: { x, display: 'block' };
+			});
+			setDotIndex(pageIndexRef.current);
+		},
+		{ filterTaps: true } // click will not be executed when dragging
+	);
+
+	const handleClickNext = () => {
+		pageIndexRef.current = currentNext();
+		apiStart();
+		setDotIndex(pageIndexRef.current);
+	};
+
+	const handleClickPrev = () => {
+		pageIndexRef.current = currentPrev();
+		apiStart();
+		setDotIndex(pageIndexRef.current);
+	};
+
+	return (
+		<div className={classes.viewPager}>
+			<IconButton className={`${classes.arrowBtn} next`} onClick={handleClickNext}>
+				<ArrowForwardIcon />
+			</IconButton>
+			<IconButton className={`${classes.arrowBtn} previous`} onClick={handleClickPrev}>
+				<ArrowBackIcon />
+			</IconButton>
+			<div className={classes.pagesContainer} ref={containerRef}>
+				{springs.map(({ x, display }, i) => (
+					<animated.div className={classes.page} {...bind()} key={i} style={{ display, x }}>
+						<animated.div
+							className="image"
+							style={{ backgroundImage: `url(${pages[i].imageLink})` }}
+							onClick={() => {
+								window.open(pages[i].content, '_blank').focus();
+							}}
+						/>
+					</animated.div>
+				))}
+			</div>
+			<PageDots
+				pages={pages}
+				index={dotIndex}
+				setIndex={setDotIndex}
+				pageIndexRef={pageIndexRef}
+				action={apiStart}
+			/>
+		</div>
+	);
+}
+
+export function ViewPager_finite_scroll(props: { pages: Page[] }) {
+	const { pages = defaultPages } = props;
+	const classes = useStyle();
+	const [dotIndex, setDotIndex] = useState(0);
+	const pageIndexRef = useRef(0);
+	const containerRef = useRef(null);
+
+	const width = useWidth(containerRef);
 
 	const currentNext = (): number => (pageIndexRef.current + 1 > pages.length - 1 ? 0 : pageIndexRef.current + 1);
 
