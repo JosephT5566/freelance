abb9f530954fdc8bae9ad4262c2f7df7dfa07f1c
Joseph Tseng <zxp930110@hotmail.com.tw>
2021-06-21 02:06:28 +0800

    add disabled props on `PopupAnimate`, and fix rerender bug with useCallback

--- src/components/shared/popupAnimate.tsx
+++ src/components/shared/popupAnimate.tsx
@@ -1,29 +1,35 @@
-import React, { useState, useEffect, useRef, ReactNode } from 'react';
+import React, { useState, useEffect, useRef, useCallback, ReactNode } from 'react';
 import { useSpring, animated } from '@react-spring/web';
 import { debounce } from '../../utils/helpers';
 
-export default function PupupAnimate(props: { children: ReactNode }) {
-	const { children } = props;
+export default function PupupAnimate(props: { disabled?: boolean; children: ReactNode }) {
+	const { disabled = false, children } = props;
 	const [display, setDisplay] = useState(false);
 	const animateRef = useRef(null);
 
 	const springProps = useSpring({ opacity: display ? 1 : 0, y: display ? 0 : 30 });
 
-	const handleScroll = debounce(() => {
-		const { bottom, height } = animateRef.current
-			? animateRef.current.getBoundingClientRect()
-			: { bottom: 0, height: 0 };
-		setDisplay(bottom - height / 3 <= window.innerHeight);
-	});
+	const handleScroll = useCallback(
+		debounce(() => {
+			console.log('scroll');
+			const { bottom, height } = animateRef.current
+				? animateRef.current.getBoundingClientRect()
+				: { bottom: 0, height: 0 };
+
+			setDisplay(bottom - height / 3 <= window.innerHeight);
+		}),
+		[]
+	);
 
 	useEffect(() => {
-		window.addEventListener('scroll', handleScroll);
+		console.log('register scroll event');
+		!disabled && window.addEventListener('scroll', handleScroll);
 
 		return () => window.removeEventListener('scroll', handleScroll);
-	}, [handleScroll]);
+	}, [handleScroll, disabled]);
 
 	return (
-		<animated.div ref={animateRef} style={{ ...springProps }}>
+		<animated.div ref={animateRef} style={disabled ? { opacity: 1 } : { ...springProps }}>
 			{children}
 		</animated.div>
 	);
