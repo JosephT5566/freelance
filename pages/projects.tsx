522545174e71a369e60bff354e976c7157246c2a
Joseph Tseng <zxp930110@hotmail.com.tw>
2021-06-20 19:40:45 +0800

    modify the website meta tags

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,6 +3,7 @@ import { makeStyles, useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import _ from 'lodash';
 
+import Head from 'next/head';
 import Project from '../src/views/payload/project';
 
 const useStyle = makeStyles((theme) => ({
@@ -67,15 +68,16 @@ export default function Projects() {
 
 	return (
 		<div className={classes.projectPage}>
+			<Head>
+				<title>Projects</title>
+			</Head>
 			<div className={classes.projects} ref={projsContRef} style={{ transform: `translateX(${transX}px)` }}>
 				<Project
 					title={'Megaport selector'}
-					image={'https://live.staticflickr.com/65535/51049862331_067e9dcec9_b.jpg'}
-					description={
-						'Nunc accumsan ipsum vitae ex lacinia varius. Donec condimentum sodales odio eu accumsan. Sed quis velit condimentum, vehicula ex sit amet, rutrum mi'
-					}
-					url={''}
-					tools={['1', '2']}
+					image={'https://i.imgur.com/OgYq1S8.png'}
+					description={'2021大港開唱，看團選擇器'}
+					url={'https://josepht5566.github.io/musicFest'}
+					tools={['React js', 'RWD web', 'Gh-pages']}
 				/>
 				<Project
 					title={'Megaport selector'}
