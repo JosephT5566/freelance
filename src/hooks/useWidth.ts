18dcd9ca8c309505a16ce9d2200f8c265c545f73
Your JosephTseng <zxp930110@hotmail.com.tw>
2021-08-09 14:55:07 +0800

    add useWidth hook

--- src/hooks/useWidth.ts
+++ src/hooks/useWidth.ts
@@ -3,9 +3,16 @@ import { useEffect, useState } from 'react';
 const useWidth = (ref: React.MutableRefObject<any>): number => {
 	const [width, setWidth] = useState(0);
 
+	const updateWidth = () => {
+		ref.current && setWidth(ref.current.clientWidth);
+	};
+
 	useEffect(() => {
-		return !!ref.current && setWidth(ref.current.clientWidth);
-	}, [ref.current?.clientWidth]);
+		updateWidth();
+		window.addEventListener('resize', updateWidth);
+
+		return () => window.removeEventListener('resize', updateWidth);
+	}, []);
 
 	return width;
 };
